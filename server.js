04c8ee9a3ba422af091836b1af525236c8fec3ab
ritushnamaharjan2-glitch <ritushnamaharjan2@gmail.com>
2025-12-08 16:20:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-// Basic 
+// Basic middle
 app.use(cors());
 app.use(express.json());
 
