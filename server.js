ad399a3de281234c1921fede0edc957b44e5906f
ritushnamaharjan2-glitch <ritushnamaharjan2@gmail.com>
2025-12-08 16:22:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 
-// Static image handler
+// Static image 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const imagesPath = path.join(__dirname, "images");
